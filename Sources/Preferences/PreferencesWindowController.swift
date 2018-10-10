53bd2d23ea0082b30eb21d189bde59626010adf3
Sindre Sorhus <sindresorhus@gmail.com>
2018-10-10 10:22:45 +0700

    Update PreferencesWindowController.swift

--- Sources/Preferences/PreferencesWindowController.swift
+++ Sources/Preferences/PreferencesWindowController.swift
@@ -20,13 +20,13 @@ public final class PreferencesWindowController: NSWindowController {
 		window.title = String(System.localizedString(forKey: "Preferences…").dropLast())
 		window.contentView = tabViewController.view
 
-		tabViewController.tabViewItems = viewControllers.map({ viewController in
+		tabViewController.tabViewItems = viewControllers.map { viewController in
 			let item = NSTabViewItem(identifier: viewController.toolbarItemTitle)
 			item.label = viewController.toolbarItemTitle
 			item.image = viewController.toolbarItemIcon
 			item.viewController = viewController as? NSViewController
 			return item
-		})
+		}
 		tabViewController.tabStyle = .toolbar
 		tabViewController.transitionOptions = [.crossfade, .slideDown]
 	}
