981ce8230a894e3a42ddaed333ddcea438991461
Peter Wunder <hallo@peterwunder.de>
2019-04-12 12:15:02 +0200

    Adds ellipsized variants

--- Sources/Preferences/util.swift
+++ Sources/Preferences/util.swift
@@ -1,17 +1,31 @@
 import Cocoa
 
 class System {
-	private static let preferencesLocalized: [String: String] = [ "ar": "تفضيلات", "ca": "Preferències", "cs": "Předvolby", "da": "Indstillinger", "de": "Einstellungen", "el": "Προτιμήσεις", "en": "Preferences", "en_AU": "Preferences", "en_GB": "Preferences", "es": "Preferencias", "es_419": "Preferencias", "fi": "Asetukset", "fr": "Préférences", "fr_CA": "Préférences", "he": "העדפות", "hi": "प्राथमिकता", "hr": "Postavke", "hu": "Beállítások", "id": "Preferensi", "it": "Preferenze", "ja": "環境設定", "ko": "환경설정", "ms": "Keutamaan", "nl": "Voorkeuren", "no": "Valg", "pl": "Preferencje", "pt": "Preferências", "pt_PT": "Preferências", "ro": "Preferințe", "ru": "Настройки", "sk": "Nastavenia", "sv": "Inställningar", "th": "การตั้งค่า", "tr": "Tercihler", "uk": "Параметри", "vi": "Tùy chọn", "zh_CN": "偏好设置", "zh_HK": "偏好設定", "zh_TW": "偏好設定" ]
+	private static let preferencesLocalized = [ "ar": "تفضيلات", "ca": "Preferències", "cs": "Předvolby", "da": "Indstillinger", "de": "Einstellungen", "el": "Προτιμήσεις", "en": "Preferences", "en_AU": "Preferences", "en_GB": "Preferences", "es": "Preferencias", "es_419": "Preferencias", "fi": "Asetukset", "fr": "Préférences", "fr_CA": "Préférences", "he": "העדפות", "hi": "प्राथमिकता", "hr": "Postavke", "hu": "Beállítások", "id": "Preferensi", "it": "Preferenze", "ja": "環境設定", "ko": "환경설정", "ms": "Keutamaan", "nl": "Voorkeuren", "no": "Valg", "pl": "Preferencje", "pt": "Preferências", "pt_PT": "Preferências", "ro": "Preferințe", "ru": "Настройки", "sk": "Nastavenia", "sv": "Inställningar", "th": "การตั้งค่า", "tr": "Tercihler", "uk": "Параметри", "vi": "Tùy chọn", "zh_CN": "偏好设置", "zh_HK": "偏好設定", "zh_TW": "偏好設定" ]
+	private static let preferencesEllipsizedLocalized = ["ar": "تفضيلات…", "ca": "Preferències…", "cs": "Předvolby…", "da": "Indstillinger…", "de": "Einstellungen…", "el": "Προτιμήσεις…", "en": "Preferences…", "en_AU": "Preferences…", "en_GB": "Preferences…", "es": "Preferencias…", "es_419": "Preferencias…", "fi": "Asetukset…", "fr": "Préférences…", "fr_CA": "Préférences…", "he": "העדפות…", "hi": "प्राथमिकता…", "hr": "Postavke…", "hu": "Beállítások…", "id": "Preferensi…", "it": "Preferenze…", "ja": "環境設定…", "ko": "환경설정...", "ms": "Keutamaan…", "nl": "Voorkeuren…", "no": "Valg…", "pl": "Preferencje…", "pt": "Preferências…", "pt_PT": "Preferências…", "ro": "Preferințe…", "ru": "Настройки…", "sk": "Nastavenia…", "sv": "Inställningar…", "th": "การตั้งค่า…", "tr": "Tercihler…", "uk": "Параметри…", "vi": "Tùy chọn…", "zh_CN": "偏好设置…", "zh_HK": "偏好設定⋯", "zh_TW": "偏好設定⋯"]
 
-	static func localizedPreferences(for locale: Locale = Locale.current) -> String {
+	static func localizedPreferences(ellipsized: Bool = false) -> String {
+		let locale = Locale.current
 		if let languageCode = locale.languageCode, let regionCode = locale.regionCode {
 			let identifier = "\(languageCode)_\(regionCode)"
 
-			if let localizedString = System.preferencesLocalized[identifier] {
-				return localizedString
-			}
-			if let localizedString = System.preferencesLocalized[languageCode] {
-				return localizedString
+			if ellipsized {
+				if let localizedString = System.preferencesEllipsizedLocalized[identifier] {
+					return localizedString
+				}
+				if let localizedString = System.preferencesEllipsizedLocalized[languageCode] {
+					return localizedString
+				}
+
+				// fallback to English locale
+				return System.preferencesEllipsizedLocalized["en"]!
+			} else {
+				if let localizedString = System.preferencesLocalized[identifier] {
+					return localizedString
+				}
+				if let localizedString = System.preferencesLocalized[languageCode] {
+					return localizedString
+				}
 			}
 		}
 
