5215d503da41154b8c94c58c7631a18b25073ab9
Christian Tietze <me@christiantietze.de>
2022-12-31 11:46:39 +0100

    add deprecations for compatibility until next major version

--- Sources/Preferences/Settings.swift
+++ Sources/Preferences/Settings.swift
@@ -5,7 +5,35 @@ public enum Settings {}
 
 // TODO: Remove in the next major version.
 // Preserve backwards compatibility.
+@available(*, deprecated, renamed: "Settings")
 public typealias Preferences = Settings
+@available(*, deprecated, renamed: "SettingsPane")
 public typealias PreferencePane = SettingsPane
+@available(*, deprecated, renamed: "SettingsPaneConvertible")
 public typealias PreferencePaneConvertible = SettingsPaneConvertible
+@available(*, deprecated, renamed: "SettingsWindowController")
 public typealias PreferencesWindowController = SettingsWindowController
+
+@available(macOS 10.15, *)
+extension Settings.Pane {
+	@available(*, deprecated, renamed: "asSettingsPane()")
+	public func asPreferencePane() -> PreferencePane {
+		asSettingsPane()
+	}
+}
+
+extension SettingsWindowController {
+	@available(*, deprecated, renamed: "init(settingsPanes:style:animated:hidesToolbarForSingleItem:)")
+	public convenience init(
+		preferencePanes: [PreferencePane],
+		style: Settings.Style = .toolbarItems,
+		animated: Bool = true,
+		hidesToolbarForSingleItem: Bool = true
+	) {
+		self.init(
+			settingsPanes: preferencePanes,
+			style: style,
+			animated: animated,
+			hidesToolbarForSingleItem: hidesToolbarForSingleItem)
+	}
+}
