d9b8aa5d58101601b63710fa462fffa6b3ea33b5
metacodes <felix.fl.liu@gmail.com>
2023-05-17 19:19:20 +0800

    use nswindow.center() to center window
    Sets the window’s location to the center of the screen. The window is placed exactly in the center horizontally and somewhat above center vertically.

--- Sources/Settings/SettingsWindowController.swift
+++ Sources/Settings/SettingsWindowController.swift
@@ -102,16 +102,12 @@ public final class SettingsWindowController: NSWindowController {
 
 	private func restoreWindowPosition() {
 		guard
-			let window,
-			let screenContainingWindow = window.screen
+			let window
 		else {
 			return
 		}
 
-		window.setFrameOrigin(CGPoint(
-			x: screenContainingWindow.visibleFrame.midX - window.frame.width / 2,
-			y: screenContainingWindow.visibleFrame.midY - window.frame.height / 2
-		))
+		window.center()
 		window.setFrameUsingName(.settings)
 		window.setFrameAutosaveName(.settings)
 	}
