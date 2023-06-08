6647eddf824f403c76921883d2225e27a0b0a1f9
ShikiSuen <shikisuen@outlook.com>
2023-06-08 10:11:52 +0800

    Also check compiler versions.

--- Sources/Settings/SettingsWindowController.swift
+++ Sources/Settings/SettingsWindowController.swift
@@ -97,7 +97,7 @@ public final class SettingsWindowController: NSWindowController {
 
 		showWindow(self)
 		restoreWindowPosition()
-		#if canImport(AppKit, _version: "14.0")
+		#if compiler(>=5.9) && canImport(AppKit, _version: "14.0")
 		if #available(macOS 14.0, *) {
 			NSApp.activate()
 		} else {
